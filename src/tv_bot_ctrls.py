206000a86147a738b208da15fd1973f53e9a9867
Del:Tango <alvearesolutions@gmail.com>
2023-01-22 08:34:26 -0500

    Code cleanup session.

--- src/tv_bot_ctrls.py
+++ src/tv_bot_ctrls.py
@@ -8,21 +8,23 @@ import asyncio
 
 log = logging.getLogger('TeleVision')
 
-# TODO
+# [ NOTE ]: Put you custom bot command functions here and don't forget to add
+# them to the BOT_CTRL_INDEX dictionary.
+
 async def start(update, context):
     '''
     [ NOTE ]: Function that will be called when the /start command is received
     '''
-    log.debug('TODO - FIX ME')
+    log.debug('')
     await update.message.reply_text('Welcome to TeleVision!')
+
 async def help(update, context):
     '''
     [ NOTE ]: Function that will be called when the /help command is received
     '''
-    log.debug('TODO - FIX ME')
-    await update.message.reply_text(
-        "Smile! You're on TeleVision!"
-    )
+    log.debug('')
+    await update.message.reply_text("Smile! You're on TeleVision!")
+
 async def error(update, context):
     '''
     [ NOTE ]: Log Errors caused by Updates.
@@ -30,6 +32,7 @@ async def error(update, context):
     log.warning(
         'Update ({}) caused error ({})'.format(update, context.error)
     )
+
 async def echo(update, context):
     await update.message.reply_text(update.message.text)
 
